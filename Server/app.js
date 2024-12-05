b0993a4d9d1132ca477c4ba08a975fae5fa63eb9
Ajay AV <avajay1999@gmail.com>
2024-12-05 22:31:32 +0530

    one orgin

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://entri-ecom-adminclient.vercel.app"],
+    origin: "https://entri-ecom-adminclient.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
