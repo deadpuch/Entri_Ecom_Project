d084b6544eb6484252805575666df313ed85ff1f
Ajay AV <avajay1999@gmail.com>
2024-12-05 22:34:52 +0530

    undo cors

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://entri-ecom-adminclient.vercel.app",
+    origin: ["http://localhost:5173","https://entri-ecom-adminclient.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
