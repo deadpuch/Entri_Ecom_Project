b66f0f386da52b466cf2a1542f8cdcc5866fe1ba
Ajay AV <avajay1999@gmail.com>
2024-12-05 21:31:18 +0530

    vercel change

--- Server/app.js
+++ Server/app.js
@@ -19,6 +19,10 @@ app.use(express.json());
 
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
